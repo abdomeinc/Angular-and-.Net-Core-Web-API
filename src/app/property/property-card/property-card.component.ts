540d16b76f9634b07e2cfd1e633368ff7b82c7b7
Ali Al-Masry <Abdo.Me.Inc@hotmail.com>
2023-07-14 22:09:53 +0300

    updating project name

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -6,10 +6,12 @@ import { Component } from "@angular/core";
     styleUrls: ['./property-card-component.css']
 })
 export class PropertyCardComponent {
-    property: any = {
-        "Id": 1,
-        "Name": "Birla House",
-        "Type": "House",
-        "Price": 12000
-    }
+    properties = [
+        { "Id": 1, "Name": "Birla House", "Type": "House", "Price": 12000 },
+        { "Id": 2, "Name": "Erose Villa", "Type": "House", "Price": 14000 },
+        { "Id": 3, "Name": "Mark Hill", "Type": "Flat", "Price": 12500 },
+        { "Id": 4, "Name": "Gun Hill", "Type": "Duplex", "Price": 11400 },
+        { "Id": 5, "Name": "Macro House", "Type": "House", "Price": 14300 },
+        { "Id": 6, "Name": "Pearl White House", "Type": "House", "Price": 18000 },
+    ]
 }
